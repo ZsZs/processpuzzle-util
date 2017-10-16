e30bbfd98ec8091e84a24588cad2ed4d66ca34cc
ZsZs <zsolt.zsuffa@gmail.com>
2017-10-16 17:50:51 +0200

    Initial files.

---
+++ processpuzzle-util-ui/demo/demo.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pp-util-demo-app',
+  template: '<pp-util-hello-world></pp-util-hello-world>'
+})
+export class DemoComponent {}
---
+++ processpuzzle-util-ui/src/index.ts
@@ -0,0 +1,1 @@
+export * from './process-puzzle-util-ui.module';
