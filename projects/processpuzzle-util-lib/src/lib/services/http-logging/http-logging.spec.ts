ae94fa867e230112593bf1618fd4a2d1269e629e
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2018-08-10 11:31:54 +0000

    increase test coverage

--- projects/processpuzzle-util-lib/src/lib/services/http-logging/http-logging.spec.ts
+++ projects/processpuzzle-util-lib/src/lib/services/http-logging/http-logging.spec.ts
@@ -1,30 +1,50 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpSentEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
 import { TestBed, inject, async } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HttpLoggingInterceptor } from './http-logging';
 
-describe('HttpLoggingInterceptor', () => {
+import { TreeNode } from '../../classes/tree-node/tree-node';
 
+@Injectable()
+export class AnyService {
+  ROOT_URL = `http://jsonplaceholder.typicode.com`;
+
+  constructor( private http: HttpClient ) {}
+
+  getNode(): Observable<TreeNode> {
+    return this.http.get<TreeNode>(`${this.ROOT_URL}/node`);
+  }
+}
+
+describe('HttpLoggingInterceptor', () => {
+  const expectedNode = new TreeNode( 'node1', 'node one' );
   let logger: HttpLoggingInterceptor;
   let httpMock: HttpTestingController;
+  let service: AnyService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [ HttpLoggingInterceptor ]
+      providers: [ AnyService, { provide: HTTP_INTERCEPTORS, useClass: HttpLoggingInterceptor, multi: true }]
     });
 
+    service = TestBed.get( AnyService );
     logger = TestBed.get( HttpLoggingInterceptor );
     httpMock = TestBed.get( HttpTestingController );
   });
 
-  it('health check: should be injected', inject([HttpLoggingInterceptor], (service: HttpLoggingInterceptor) => {
-    expect( service ).toBeTruthy();
-  }));
+  it('should log HTTP requests', inject( [HttpTestingController, AnyService], (httpMock: HttpTestingController, service: AnyService) => {
+    service.getNode().subscribe( response => {
+      expect( true ).toBeTruthy();
+    });
+    
+    const httpRequest = httpMock.expectOne(`${service.ROOT_URL}/node`);
 
-  it('intercept() logs HTTP requests', () => {
-    expect( true ).toBeTruthy();
-  });
+    httpRequest.flush( expectedNode );
+    httpMock.verify();
+  }));
 });
