7d643b52db841dc56455ac3a7381cfcbfa0291ba
Zsolt Zsuffa <zsolt.zsuffa@hpe.com>
2018-09-27 12:09:44 +0200

    experiment with version bump

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,22 +1,22 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "v0.1.2-4-gcf5b485-dirty",
-    "hash": "gcf5b485",
-    "distance": 4,
-    "tag": "v0.1.2",
+    "dirty": false,
+    "raw": "v0.1.3-0-g12ec48b",
+    "hash": "g12ec48b",
+    "distance": 0,
+    "tag": "v0.1.3",
     "semver": {
-        "raw": "v0.1.2",
+        "raw": "v0.1.3",
         "major": 0,
         "minor": 1,
-        "patch": 2,
+        "patch": 3,
         "prerelease": [],
         "build": [],
-        "version": "0.1.2"
+        "version": "0.1.3"
     },
-    "suffix": "4-gcf5b485-dirty",
-    "semverString": "0.1.2+4.gcf5b485",
+    "suffix": "0-g12ec48b",
+    "semverString": "0.1.3",
     "version": "0.1.3"
 };
 /* tslint:enable */
